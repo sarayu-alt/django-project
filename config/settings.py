f37096951e4be40194f838263d9ed0eaffe7aed9
Navvothu Sarayu <sarayunavvothu@gmail.com>
2026-07-07 14:27:09 +0530

    Fixed ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = 'django-insecure-fvrml-wx7g1x70m#ol%#!%k^7)uz4i5uqelgorin#f2f2e7@!n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["django-project-uy1n.onrender.com",
+    "127.0.0.1",
+    "localhost",
+    ]
 
 
 # Application definition
